88ee3b451294f13ca0e2d9a34b4f73ab529e817a
Ishanax <67413079+Ishanax@users.noreply.github.com>
2021-12-15 11:39:49 +0100

    randomly filled playing field with bombs and empty squares

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
     const grid = document.querySelector(".grid");
+    let bombAmount = 25;
     let width = 10;
     let squares = [];
 
+    
     createBoard = () => {
+        //random bombs:
+        const bombsArray = Array(bombAmount).fill("bomb"); //fill fills whole array with bomb
+        const emptyArray = Array(width*width - bombAmount).fill("empty"); 
+        console.log(bombsArray);
+        console.log(emptyArray);
+        const joinBombsAndEmptyArray = emptyArray.concat(bombsArray);
+        console.log(joinBombsAndEmptyArray);
+        const randomGameArray = joinBombsAndEmptyArray.sort(()=> Math.random() -0.5);
+        
+        console.log(randomGameArray);
+
+        //make board:
         for(let i = 0; i < width*width; i++) {
             const square = document.createElement("div");
             square.setAttribute("id", i);
